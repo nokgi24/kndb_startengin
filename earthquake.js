505f0b0cb0ee588e515bc0c545c2220c3cf3ce68
nokgi <103976507+nokgi24@users.noreply.github.com>
2024-08-17 11:43:34 +0900

    Update earthquake.js

--- earthquake.js
+++ earthquake.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import request from 'request';
 import convert from 'xml-js';
 
-const serviceKey = 'M32SirZ7RxVajtKade1bvVHMsgQauKkVdVVL3q9uRXfDg0Xs0GBqeBUQ554GA8LrS7F%2FLF1KrgKEGFb8rayaSA%3D%3D';
+const serviceKey = process.env.Services_KEY;
 
 
 function getPastTimeISOString(days) {
