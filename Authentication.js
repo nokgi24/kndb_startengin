06e3b8d17407958c535d59da63df129c130f8755
nokgi <103976507+nokgi24@users.noreply.github.com>
2024-08-17 21:39:22 +0900

    Update Authentication.js

--- Authentication.js
+++ Authentication.js
@@ -1,11 +1,5 @@
-//인증 js
-
-const API_BASE_URL = 'https://discord.com/api/v14/';
-
-// 환경 변수 유효성 검사
-if (!process.env.DISCORD_TOKEN) {
-  throw new Error('Missing DISCORD_TOKEN in environment variables');
-}
+// Authentication.js
+import { verifyKey } from 'discord-interactions';
 
 // Discord 요청 검증 함수
 export function VerifyDiscordRequest(clientKey) {
@@ -22,29 +16,3 @@ export function VerifyDiscordRequest(clientKey) {
   };
 }
 
-
-export async function DiscordRequest(endpoint, options) {
-  const url = `${API_BASE_URL}${endpoint}`;
-  
-  // Payload가 있는 경우 JSON 문자열로 변환
-  if (options.body) options.body = JSON.stringify(options.body);
-
-  // node-fetch를 사용하여 요청 실행
-  const res = await fetch(url, {
-    headers: {
-      Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
-      'Content-Type': 'application/json; charset=UTF-8',
-      'User-Agent': 'DiscordBot (https://github.com/discord/discord-example-app, 1.0.0)',
-    },
-    ...options,
-  });
-
-  // API 오류 처리
-  if (!res.ok) {
-    const data = await res.json();
-    console.error(`Discord API error: ${res.status} - ${data.message}`);
-    throw new Error(JSON.stringify(data));
-  }
-
-  return res;
-}
