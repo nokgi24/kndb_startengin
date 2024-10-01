85fbd7f444c3269542d3f6d61ed14628c63c2bfe
nokgi <103976507+nokgi24@users.noreply.github.com>
2024-10-01 23:01:03 +0900

    Update app.js

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ async function handleEarthquakeUpdate() {
         const dep = transformedData[0].dep || '정보 없음';
         const tmFc = transformedData[0].tmFc || '정보 없음';
         const loc = transformedData[0].loc || '정보 없음';
-	const img = transgormedData[0].img || 'no data';
+	const img = transformedData[0].img || 'https://cdn.pixabay.com/photo/2017/06/08/17/32/not-found-2384304_1280.jpg';
 
         const formattedTmFc = tmFc !== '정보 없음' ? formatDate(tmFc) : '정보 없음';
     } catch (error) {
@@ -152,7 +152,7 @@ async function handleEarthquakeUpdate() {
                 timestamp: new Date(),
                 color: color_x,
 		image: {
-		url: '${img}',
+		url: img,
 		},
                 footer: {
                   text: '기상청_kma제공'
