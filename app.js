106851ace2c007d3b564b147965820531153ff20
nokgi <103976507+nokgi24@users.noreply.github.com>
2024-10-01 19:52:47 +0900

    Update app.js

--- app.js
+++ app.js
@@ -23,23 +23,69 @@ let dep = '';
 let tmFc = '';
 let loc = '';
 
-// Discord client 설정
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, readyClient => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-// Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
 
-// 일반적인 요청에 대해서는 express.json() 사용
 app.use('/protected', verifyRequest);
 app.use('/api', express.json());
 
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
+
+function getEarthquakeMessage(data_system) {
+  let title, description, color_x;
+
+  switch (data_system) {
+    case '2':
+      title = '[국외지진정보]';
+      description = '국외 지진정보가 발표되었습니다. 해당지역에서는 주의하시기 바랍니다.';
+      color_x = 0xfd2b2b;
+      break;
+    case '3':
+      title = '[국내지진정보]';
+      description = '국내 지진정보가 발표되었습니다. 해당지역에서는 강한 흔들림에 주의하시기 바랍니다.';
+      color_x = 0xece632;
+      break;
+    case '5':
+      title = '[국내지진정보(재통보)]';
+      description = '국내 지진정보(재통보)가 발표되었습니다. 확인 후 주의하시기 바랍니다.';
+      color_x = 0xece632;
+      break;
+    case '11':
+      title = '[국내지진조기경보]';
+      description = '국내 지진조기경보가 발표되었습니다. 해당지역에서는 강한 흔들림에 주의하시기 바랍니다.';
+      color_x = 0xfd2b2b;
+      break;
+    case '12':
+      title = '[국외지진조기경보]';
+      description = '국외 지진조기경보가 발표되었습니다. 주의하시기 바랍니다.';
+      color_x = 0xfd2b2b;
+      break;
+    case '13':
+      title = '[조기경보 정밀분석]';
+      description = '조기경보 정밀분석 정보입니다. 지진 발생 가능성에 유의하세요.';
+      color_x = 0xece632;
+      break;
+    case '14':
+      title = '[조기경보 조기분석]';
+      description = '지진속보(조기분석) 정보입니다. 최신 정보를 확인하세요.';
+      color_x = 0xece632;
+      break;
+    default:
+      title = '[현재발생지진없음]';
+      description = '현재 지진 정보가 없습니다.';
+      color_x = 0x00ff00;
+      break;
+  }
+
+  return { title, description, color_x };
+}
 
 async function handleEarthquakeUpdate() {
   try {
@@ -48,14 +94,65 @@ async function handleEarthquakeUpdate() {
     if (['2', '3', '5', '11', '12', '13', '14'].includes(data_system)) {
       data_system_1 = data_system;
       same = 0;
-    } else {
-      same = 1;
+      try{
+        const { title, description, color_x } = getEarthquakeMessage(data_system);
+
+        const mt = transformedData[0].mt || '정보 없음';
+        const inT = transformedData[0].inT || '정보 없음';
+        const dep = transformedData[0].dep || '정보 없음';
+        const tmFc = transformedData[0].tmFc || '정보 없음';
+        const loc = transformedData[0].loc || '정보 없음';
+
+        const formattedTmFc = tmFc !== '정보 없음' ? formatDate(tmFc) : '정보 없음';
+    } catch (error) {
+      console.error('Error processing earthquake data:', error);
+      title = '[오류]';
+      description = '지진 정보 처리 중 오류가 발생했습니다.';
+      color_x = 0xff0000;
+    }
+
+      const fields = [
+        { name: 'M', value: mt, inline: true },
+        { name: '깊이', value: dep, inline: true },
+        { name: '발표시각', value: formattedTmFc, inline: true },
+        { name: '위치', value: loc, inline: true }
+      ];
+
+      if (data_system !== '12') {
+        fields.splice(1, 0, { name: '최대 측정 진도', value: inT, inline: true });
+      }
+
+      if (selectedChannelId) {
+        const channel = client.channels.cache.get(selectedChannelId);
+        if (channel) {
+          await channel.send({
+            embeds: [
+              {
+                title: title,
+                description: description,
+                fields: fields,
+                timestamp: new Date(),
+                color: color_x,
+                footer: {
+                  text: '기상청_kma제공'
+                }
+              }
+            ]
+          });
+        } else {
+          console.error('지정된 채널을 찾을 수 없습니다.');
+        }
+      } else {
+        console.log('채널이 지정되지 않았습니다.');
+      }
+      } else {
+        console.log('현재 지진 정보가 없습니다.');
+      }
+   } catch (error) {
+      console.error('Error updating earthquake information:', error);
     }
     console.log("datasystem_1:", data_system_1);
-  } catch (error) {
-    console.error('Error updating earthquake information:', error);
   }
-}
 
 setInterval(handleEarthquakeUpdate, 10000);
 
@@ -66,108 +163,13 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async (re
     console.log("pong");
     return res.send({ type: InteractionResponseType.PONG });
   }
+})
+ 
 
-  if (type === InteractionType.APPLICATION_COMMAND) {
-    const { name } = data;
-	  
-    try {
-      const transformedData = await fetchEarthquakeData();
 
-      switch (data_system_1) {
-        case '2':
-          title = '[국외지진정보]';
-          description = '국외 지진정보가 발표되었습니다. 해당지역에서는 주의하시기 바랍니다.';
-          color_x = 0xfd2b2b;
-          break;
-        case '3':
-          title = '[국내지진정보]';
-          description = '국내 지진정보가 발표되었습니다. 해당지역에서는 강한 흔들림에 주의하시기 바랍니다.';
-          color_x = 0xece632;
-          break;
-        case '5':
-          title = '[국내지진정보(재통보)]';
-          description = '국내 지진정보(재통보)가 발표되었습니다. 확인 후 주의하시기 바랍니다.';
-          color_x = 0xece632;
-          break;
-        case '11':
-          title = '[국내지진조기경보]';
-          description = '국내 지진조기경보가 발표되었습니다. 해당지역에서는 강한 흔들림에 주의하시기 바랍니다.';
-          color_x = 0xfd2b2b;
-          break;
-        case '12':
-          title = '[국외지진조기경보]';
-          description = '국외 지진조기경보가 발표되었습니다. 주의하시기 바랍니다.';
-          color_x = 0xfd2b2b;
-          break;
-        case '13':
-          title = '[조기경보 정밀분석]';
-          description = '조기경보 정밀분석 정보입니다. 지진 발생 가능성에 유의하세요.';
-          color_x = 0xece632;
-          break;
-        case '14':
-          title = '[조기경보 조기분석]';
-          description = '지진속보(조기분석) 정보입니다. 최신 정보를 확인하세요.';
-          color_x = 0xece632;
-          break;
-        default:
-          title = '[현재발생지진없음]';
-          description = '현재 지진 정보가 없습니다.';
-          color_x = 0x00ff00;
-          return res.send({
-            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            data: {
-              content: '',
-              embeds: [
-                {
-                  title: title,
-                  description: description,
-                  timestamp: new Date(),
-                  color: color_x,
-                }
-              ]
-            }
-          });
-      }
+    
 
-      mt = transformedData[0].mt || '정보 없음';
-      inT = transformedData[0].inT || '정보 없음';
-      dep = transformedData[0].dep || '정보 없음';
-      tmFc = transformedData[0].tmFc || '정보 없음';
-      loc = transformedData[0].loc || '정보없음';
 
-    } catch (error) {
-      console.error('Error processing earthquake data:', error);
-      title = '[오류]';
-      description = '지진 정보 처리 중 오류가 발생했습니다.';
-      color_x = 0xff0000;
-    }
-
-    return res.send({
-      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-      data: {
-        content: '',
-        embeds: [
-          {
-            title: title,
-            description: description,
-            fields: [
-              { name: 'M', value: mt, inline: true },
-              { name: '최대 측정 진도', value: inT, inline: true },
-              { name: '깊이', value: dep, inline: true },
-              { name: '발표시각 ', value: tmFc, inline: true },
-              { name: '위치 ', value: loc, inline: true }
-            ],
-            timestamp: new Date(),
-            color: color_x,
-            footer: {
-              text: '기상청_kma제공'
-            }
-          }
-        ]
-      }
-    });
-  }
-});
 
 app.listen(PORT, () => {
   console.log('Listening on port', PORT);
