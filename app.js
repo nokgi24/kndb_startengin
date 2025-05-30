c880f8b8ebfbd28e9cf152d3ef5a998e7ddb5412
nokgi <103976507+nokgi24@users.noreply.github.com>
2025-05-30 20:51:09 +0900

    Update app.js

--- app.js
+++ app.js
@@ -35,6 +35,9 @@ app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 
 function getEarthquakeMessage(data_system) {
