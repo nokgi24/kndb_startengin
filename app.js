819aee6225e3c9b2c88caa16e39019441508c5fb
nokgi <103976507+nokgi24@users.noreply.github.com>
2025-05-30 21:01:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ import { registerCommands } from './commands.js';
 import moment from "moment";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 let same = 0;
 let selectedChannelId = null;
 const guildChannelMap = {};
